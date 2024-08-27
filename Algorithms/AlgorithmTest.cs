8dfc86e3ffbccdcd05925bae3a868092908ba007
Greg Gadowski <gregga@mohela.com>
2024-08-27 15:09:47 -0500

    Algorithms.cs code implementation

--- Algorithms/AlgorithmTest.cs
+++ Algorithms/AlgorithmTest.cs
@@ -4,16 +4,18 @@ namespace DeveloperSample.Algorithms
 {
     public class AlgorithmTest
     {
-        [Fact(Skip="Not implemented")]
+        [Fact]
         public void CanGetFactorial()
         {
             Assert.Equal(24, Algorithms.GetFactorial(4));
         }
 
-        [Fact(Skip="Not implemented")]
+        [Fact]
         public void CanFormatSeparators()
         {
             Assert.Equal("a, b and c", Algorithms.FormatSeparators("a", "b", "c"));
+            Assert.Equal("a and b", Algorithms.FormatSeparators("a", "b"));
+            Assert.Equal("a", Algorithms.FormatSeparators("a"));
         }
     }
 }
--- Algorithms/Algorithms.cs
+++ Algorithms/Algorithms.cs
@@ -1,11 +1,30 @@
 using System;
+using System.Linq;
 
 namespace DeveloperSample.Algorithms
 {
     public static class Algorithms
     {
-        public static int GetFactorial(int n) => throw new NotImplementedException();
+        public static int GetFactorial(int n)
+        {
+            int factorial = 1;
+            for (var i = 1; i <= n; i++)
+            {
+                factorial *= i;
+            }
+            return factorial;
+        }
 
-        public static string FormatSeparators(params string[] items) => throw new NotImplementedException();
+        public static string FormatSeparators(params string[] items)
+        {
+            if(items.Count() >= 2) 
+            {
+                return $"{string.Join(", ", items, 0, items.Count() - 1)} and {items.Last()}";
+            }
+            else
+            {
+                return $"{items.Last()}";
+            }
+        }
     }
 }
